872d9c373bf221e3b9a98fca4c65827abd947a13
MariaMurra <47236003+MariaMurra@users.noreply.github.com>
2019-02-07 21:36:19 -0800

    Assignment 1
    Putting JS to practice

---
+++ JS/script.js
@@ -0,0 +1,156 @@
+/*eslint-env browser*/
+/*STEP 1
+
+var someMonth;
+function theMonth() ;
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+*/
+
+//STEP 2
+
+/*numeric literal expression
+w = 396;*/
+
+/*string literal expression 
+
+"when you worry, you make it double.";
+*/
+
+/* Boolean expression
+true
+*/
+
+/* Null expression
+null*/
+
+//STEP 3
+/*var sanDiego = 4 + (3 * 3) / 2;
+var myCar = sanDiego (sanDiego + 24) % 10;*/
+
+//STEP 4
+/*var strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, boolMayWeContactYou;*/
+
+//STEP 5 
+/* var strFirstName = "Maria";
+var strLastName = "Murra";
+var strAddress = "123 my street";
+var strCity;
+strCity = "Nasty City";
+var strState;
+strState = "CA";
+var numZipCode;
+numZipCode = 91922;
+var numYourAge = 50, strReferralSource = "Catalog", boolMayWeContactYou = yes; 
+*/
+
+//STEP 6
+/*var myConsole1 = 5;
+window.console.log("remember, remember the " + myConsole1 + " th of November");*/
+
+
+//STEP 7
+/*var first = 4;
+var second = 10;
+var third = first > second;
+window.console.log("hi there this is a weird relation " + third);
+
+var tryAgain = 275.900812;
+var tryAgain2 =  358;
+var tres = (tryAgain > tryAgain2);
+window.console.log(999 + tres);*/
+
+//STEP 8
+/*var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);*/
+
+// STEP 9
+/*var t = 600;
+var u = null;
+var w = t + u;
+window.console.log(u);*/
+
+//STEP 10
+/*window.console.log(typeof "Mi mamá me mima");
+var y = 1974596;
+window.console.log(typeof y);
+window.console.log(typeof a);
+var a = new Object();
+a.alphabet = "first";
+a.coordinates = "Square one";
+window.console.log(typeof true);
+window.console.log(typeof null);*/
+
+//STEP 11
+/*window.alert("Hello" + " Maria Murra" + ", welcome to the JavaScript class!");*/
+
+//STEP 12
+/*var name = "Maria";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+//STEP 13
+/*var name = "Maria";
+var course = "Javascript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+//STEP 14
+/*var name = "Maria";
+var course = "Javascript";
+window.alert("Hello " + name + ", \nWelcome to the " + course + " class!");*/
+
+//STEP 15
+/*var name = window.prompt("Hi! what's your name?");
+var course = "Javascript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+//STEP 16
+/*var name = window.prompt("Hi! what\'s your name?");
+var course = window.prompt("What\'s your Class?");
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+//STEP 17
+/*var x, y;
+x = 10;
+y = 20;
+window.console.log(x + y);*/
+
+//STEP 18
+/*var x = 20;
+x = x + 20;
+window.console.log(x);*/
+
+//STEP 19
+/*var x = 20;
+x = x *= 5;
+window.console.log(x);*/
+
+//STEP 20
+
+/*var x = 20 % 3;
+x = x /= 1;
+window.console.log(x);*/
+
+
+
+//STEP 21
+/*var x = 5;
+var y = 60;
+window.console.log(x == 6 || y != 50);*/
+
+//STEP 22
+/*var x = 5;
+var y = 60;
+window.console.log(x == 6 && y != 50);*/
+
+//STEP 23
+/* var widget = new Object();
+window.console.log(typeof widget);*/
+
+//STEP 24
+/*var widget = new Object();
+window.console.log(widget instanceof Object);*/
+
+//STEP 25
+/*var widget = new Object();
+window.console.log(null instanceof Object);*/
